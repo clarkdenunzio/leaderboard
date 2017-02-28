e0563c75828225575d6c111dcde1e70abc55ad1d
clarkdenunzio <zhuclarkx205@yahoo.com>
2017-02-28 11:40:08 -1000

    Task 4: Templates

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -2,3 +2,16 @@
  * Created by clark on 2/28/17.
  * JS file for leaderboard.
  */
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    }
+  });
+}
+
+if(Meteor.isServer){
+  // this code only runs on the server
+}
